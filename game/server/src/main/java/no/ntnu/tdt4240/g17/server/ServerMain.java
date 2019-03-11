aeed391b6390dd4017322adf92c45ea8d6bd536a
Kristian Rekstad <krirek@msn.com>
2019-03-11 14:47:01 +0100

    Add logging with SLF4J. Add Lombok for @SLF4J.
    Notice: install lombok plugin

--- game/server/src/main/java/no/ntnu/tdt4240/g17/server/ServerMain.java
+++ game/server/src/main/java/no/ntnu/tdt4240/g17/server/ServerMain.java
@@ -1,7 +1,20 @@
 package no.ntnu.tdt4240.g17.server;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Main class for the server.
  */
-public class ServerMain {
+@Slf4j
+public final class ServerMain {
+    /** Hidden constructor for utility classes. */
+    private ServerMain() { }
+
+    /**
+     * Starts the server.
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        log.error("Testing");
+    }
 }
