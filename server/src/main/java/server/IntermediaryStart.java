862110e9e945decfd7abb433c5a3b37e24faca74
Morten Bujordet <mo.bujordet@gmail.com>
2019-03-11 15:26:13 +0100

    Add server

---
+++ server/src/main/java/server/IntermediaryStart.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+
+public class IntermediaryStart {
+    public ArrayList playerIds;
+    public ArrayList playerNames;
+    public Dictionary newScores;
+    public Integer roundNumber;
+    public String nextArena;
+    public String startingPositions;
+    public String gameMode;
+}
---
+++ server/src/main/java/server/PlayMessage.java
@@ -0,0 +1,8 @@
+package server;
+
+public class PlayMessage {
+    public String id;
+    public String name;
+    public String gameMode;
+
+}
