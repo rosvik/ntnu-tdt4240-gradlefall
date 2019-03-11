3c35974dd6282261e3f87bb21b8a2afad26b9373
Kristian Rekstad <krirek@msn.com>
2019-03-11 13:20:07 +0100

    Fix checkstyle

--- game/desktop/src/main/java/no/ntnu/tdt4240/g17/cool_game/desktop/DesktopLauncher.java
+++ game/desktop/src/main/java/no/ntnu/tdt4240/g17/cool_game/desktop/DesktopLauncher.java
@@ -4,9 +4,18 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import no.ntnu.tdt4240.g17.cool_game.MainGame;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(), config);
-	}
+/**
+ * Starts the desktop version of the game.
+ */
+public final class DesktopLauncher {
+    /** Hidden constructor. */
+    private DesktopLauncher() { }
+
+    /** Main method for desktop.
+     * @param arg command line arguments
+     */
+    public static void main(final String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MainGame(), config);
+    }
 }
