df47e3d01f50891fe0d778994743e5736cdc2f40
Kristian Rekstad <krirek@msn.com>
2019-03-18 17:38:16 +0100

    Add system to wrap players and projectiles that exit the arena

--- game/server/src/main/java/no/ntnu/tdt4240/g17/server/physics/box2d/BoundingBoxComponent.java
+++ game/server/src/main/java/no/ntnu/tdt4240/g17/server/physics/box2d/BoundingBoxComponent.java
@@ -4,22 +4,27 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.gdx.math.Rectangle;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Holds a bounding box to easily find the size of an entity.
  *
  * @author Kristian 'krissrex' Rekstad
  */
+@Getter @Setter
 public class BoundingBoxComponent implements Component {
 
     /** The mapper to get this component from {@link com.badlogic.ashley.core.Entity entities}. */
     public static final ComponentMapper<BoundingBoxComponent> MAPPER =
             ComponentMapper.getFor(BoundingBoxComponent.class);
 
+    /** Bounding box for the object in local coordinates, meters. */
     private Rectangle boundingBox;
 
     /**
      * Create a new component.
-     * @param boundingBox the bounding box
+     * @param boundingBox the bounding box in local coordinates using meters.
      */
     public BoundingBoxComponent(final Rectangle boundingBox) {
         this.boundingBox = boundingBox;
