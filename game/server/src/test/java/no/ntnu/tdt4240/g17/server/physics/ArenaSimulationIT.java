925a244cb4ca92dad2241e16d333a31d04d12e87
Kristian Rekstad <krirek@msn.com>
2019-03-19 13:57:25 +0100

    Add integration test with firing many projectiles

--- game/server/src/test/java/no/ntnu/tdt4240/g17/server/physics/ArenaSimulationIT.java
+++ game/server/src/test/java/no/ntnu/tdt4240/g17/server/physics/ArenaSimulationIT.java
@@ -3,8 +3,10 @@ package no.ntnu.tdt4240.g17.server.physics;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IntervalSystem;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -17,7 +19,10 @@ import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 import no.ntnu.tdt4240.g17.server.game_engine.player.PlayerComponent;
+import no.ntnu.tdt4240.g17.server.physics.box2d.Box2dBodyComponent;
+import no.ntnu.tdt4240.g17.server.physics.box2d.Box2dBodyToTransformComponentAdapter;
 import no.ntnu.tdt4240.g17.server.physics.box2d.TransformComponent;
+import no.ntnu.tdt4240.g17.server.physics.box2d.body.ProjectileBox2dBodyFactory;
 import no.ntnu.tdt4240.g17.server.physics.util.ArenaTestUtil;
 import no.ntnu.tdt4240.g17.server.physics.util.DesktopPhysicsVisualizer;
 
@@ -71,6 +76,53 @@ public class ArenaSimulationIT {
         // Execute
         boolean useGui = true;
         log.info("Running simulation");
+        runSimulation(world, engine, useGui);
+
+        // Cleanup
+        engine.removeAllEntities();
+        world.dispose();
+    }
+
+    @Test @Disabled
+    void simulateFiringProjectiles() {
+        // Configure
+        final World world = new World(new Vector2(0, -9.81f), true);
+        final PhysicsSystem physicsSystem = new PhysicsSystem(PhysicsSystem.PHYSICS_FAMILY, 1, 0.015f, world);
+        final FireProjectileSystem fireProjectileSystem = new FireProjectileSystem(0.2f, 0,
+                new ProjectileBox2dBodyFactory(world), new Box2dBodyToTransformComponentAdapter());
+
+        final Engine engine = new Engine();
+        engine.addSystem(physicsSystem);
+        engine.addSystem(new PlayerPositionDebugSystem(2));
+        engine.addSystem(fireProjectileSystem);
+
+        // Numbers are players, - are arrows
+        final String arena =
+                "#        #\n" +
+                "#        #\n" +
+                "#        #\n" +
+                "#        #\n" +
+                "#        #\n" +
+                "#        #\n" +
+                "##########";
+
+        final List<Entity> entities = ArenaTestUtil.createComponentsFromArena(arena, world);
+        for (final Entity entity : entities) {
+            engine.addEntity(entity);
+        }
+
+
+        // Execute
+        boolean useGui = true;
+        log.info("Running simulation");
+        runSimulation(world, engine, useGui);
+
+        // Cleanup
+        engine.removeAllEntities();
+        world.dispose();
+    }
+
+    private void runSimulation(final World world, final Engine engine, final boolean useGui) {
         if (useGui) {
             final DesktopPhysicsVisualizer gui = new DesktopPhysicsVisualizer();
             log.info("Gui started");
@@ -93,10 +145,6 @@ public class ArenaSimulationIT {
                 iteration++;
             }
         }
-
-        // Cleanup
-        engine.removeAllEntities();
-        world.dispose();
     }
 
     /**
@@ -128,6 +176,41 @@ public class ArenaSimulationIT {
 
                 log.trace("Player {} is at {}", playerComponent.getId(), transformComponent.getPosition());
             }
+        }
+    }
+
+    protected static class FireProjectileSystem extends IntervalSystem {
+        private final ProjectileBox2dBodyFactory projectileFactory;
+        private final Box2dBodyToTransformComponentAdapter adapter;
+
+        private float angleDegrees = 10f;
+
+        /**
+         * @param interval time in seconds
+         */
+        public FireProjectileSystem(final float interval, final int priority,
+                                    ProjectileBox2dBodyFactory projectileFactory,
+                                    Box2dBodyToTransformComponentAdapter adapter) {
+            super(interval, priority);
+            this.projectileFactory = projectileFactory;
+            this.adapter = adapter;
+        }
+
+        @Override
+        protected void updateInterval() {
+            final Entity entity = new Entity();
+            final Body body = projectileFactory.create(entity);
+            entity.add(new Box2dBodyComponent(body));
+            entity.add(adapter.createFrom(body));
+
+            final float angle = (float) Math.toRadians(angleDegrees);
+            body.setLinearVelocity(25f*(float)Math.cos(angle), 25f*(float)Math.sin(angle));
+            body.setTransform(1.5f, 1.5f, angle);
+
+            getEngine().addEntity(entity);
+
+            angleDegrees += 10f;
+            angleDegrees = angleDegrees % 90f;
         }
     }
 }
