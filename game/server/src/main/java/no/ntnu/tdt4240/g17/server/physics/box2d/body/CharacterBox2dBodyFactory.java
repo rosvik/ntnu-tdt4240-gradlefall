ee71bab688ea4b2e105517d2d25f041389a96575
Kristian Rekstad <krirek@msn.com>
2019-03-19 13:14:43 +0100

    Elevate edges more to avoid players standing in the air

--- game/server/src/main/java/no/ntnu/tdt4240/g17/server/physics/box2d/body/CharacterBox2dBodyFactory.java
+++ game/server/src/main/java/no/ntnu/tdt4240/g17/server/physics/box2d/body/CharacterBox2dBodyFactory.java
@@ -69,10 +69,11 @@ public class CharacterBox2dBodyFactory extends BaseBox2dBodyFactory {
     protected void addShapes(final Body body) {
         final PolygonShape shape = new PolygonShape();
         // The bottom has a center point and lifted edges to avoid getting stuck between tiles.
+        final float edgeHeight = 0.1f;
         shape.set(new float[]{
-                0, 0.05f,
+                0, edgeHeight,
                 characterWidth / 2f, 0,
-                characterWidth, 0.05f,
+                characterWidth, edgeHeight,
                 characterWidth, characterHeight,
                 0, characterHeight
         });
