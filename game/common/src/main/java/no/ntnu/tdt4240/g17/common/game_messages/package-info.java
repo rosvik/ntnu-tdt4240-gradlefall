0f12f31e4a915ce89c51ef8c7943123134b940dc
Kristian Rekstad <krirek@msn.com>
2019-03-26 10:46:38 +0100

    Add package-info with link to drive

---
+++ game/common/src/main/java/no/ntnu/tdt4240/g17/common/game_messages/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Read
+ * <a href="https://docs.google.com/document/d/1F0NOSaBUzT5K2YWoiMNPRiuWdzN6DLStGYM1JTZCMRY/edit?usp=sharing">
+ *     Client Server communication</a>
+ * on our google drive.
+ */
+package no.ntnu.tdt4240.g17.common.game_messages;
+
