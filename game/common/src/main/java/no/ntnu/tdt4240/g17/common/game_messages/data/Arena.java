b9d29b9ba9ad698d0a67ac15aed1c8a45d8232ea
Kristian Rekstad <krirek@msn.com>
2019-03-26 11:02:17 +0100

    Remove map from Arena enum. This connection can be established in a Map or function instead.

--- game/common/src/main/java/no/ntnu/tdt4240/g17/common/game_messages/data/Arena.java
+++ game/common/src/main/java/no/ntnu/tdt4240/g17/common/game_messages/data/Arena.java
@@ -1,9 +1,5 @@
 package no.ntnu.tdt4240.g17.common.game_messages.data;
 
-import org.jetbrains.annotations.NotNull;
-
-import lombok.Getter;
-
 /**
  * Represents all available arenas.
  *
@@ -11,18 +7,5 @@ import lombok.Getter;
  */
 public enum Arena {
     /** */
-    ARENA_1("map2.tmx");
-
-    /** The filename for a arena tmx-file. */
-    @Getter
-    private final String arenaTmxFilePath;
-
-    /**
-     * @param arenaTmxFilePath the file for a arena tilemap.
-     */
-    Arena(@NotNull final String arenaTmxFilePath) {
-        this.arenaTmxFilePath = arenaTmxFilePath;
-    }
-
-
+    ARENA_1
 }
