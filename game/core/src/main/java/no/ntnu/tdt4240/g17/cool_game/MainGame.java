b862328c025ddb1b4e214834ebd79719f758f0b9
dennisjl <dennisjianbin@gmail.com>
2019-04-02 13:46:56 +0200

    Merge remote-tracking branch 'remotes/origin/master' into feature/dj/issue#4/navigation
    # Conflicts:
    #	game/android/build.gradle
    #	game/core/src/main/java/no/ntnu/tdt4240/g17/cool_game/MainGame.java
    #	game/core/src/test/java/no/ntnu/tdt4240/g17/cool_game/DeleteThis_CodeCoverageHackTest.java
    #	game/core/src/test/java/no/ntnu/tdt4240/g17/cool_game/MainGameTest.java
    #	game/server/src/test/java/no/ntnu/tdt4240/g17/server/physics/ArenaSimulationIT.java

--- game/core/src/main/java/no/ntnu/tdt4240/g17/cool_game/MainGame.java
+++ game/core/src/main/java/no/ntnu/tdt4240/g17/cool_game/MainGame.java
@@ -1,67 +1,28 @@
 package no.ntnu.tdt4240.g17.cool_game;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import no.ntnu.tdt4240.g17.cool_game.screens.navigation.Navigator;
-
-import no.ntnu.tdt4240.g17.cool_game.game_arena.Arena;
 
 /**
  * Main game class.
  */
 public class MainGame extends ApplicationAdapter {
-    /** Test image. */
-    private Texture img;
+
     private Navigator navigator;
-    SpriteBatch batch;
-    /** Background image. */
-    private Texture background;
-
-    /** Arena. */
-    Arena arena;
-
-    /** Arena width in tiles. */
-    private float width;
-
-    /** Arena height in tiles. */
-    private float height;
-
     @Override
     public final void create() {
-        width = 32f;
-        height = 20f;
-
-        batch = new SpriteBatch();
         navigator = new Navigator();
-        arena = new Arena("map2.tmx", 16f, width, height, batch);
-        background = new Texture("background.png");
     }
 
     @Override
     public final void render() {
-        /*Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-
-        // Draw the background
-        batch.draw(background, 0f, 0f, width, height);
-
-        // Render the arena
-        arena.renderArena();
-        arena.renderForeground();
-
-        //batch.draw(img, 0, 0);
-        batch.end();*/
         navigator.getScreen().render(Gdx.graphics.getDeltaTime());
     }
 
     @Override
     public final void dispose() {
-        if (batch != null) {
-            batch.dispose();
-        }
         if (navigator != null) {
             navigator.dispose();
         }
