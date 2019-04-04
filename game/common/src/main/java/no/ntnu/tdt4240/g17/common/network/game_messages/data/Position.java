2ae381fd36527c8d3ec0d94126e781e7b7205e81
Kristian Rekstad <krirek@msn.com>
2019-04-04 15:36:34 +0200

    Add constructor to Position

--- game/common/src/main/java/no/ntnu/tdt4240/g17/common/network/game_messages/data/Position.java
+++ game/common/src/main/java/no/ntnu/tdt4240/g17/common/network/game_messages/data/Position.java
@@ -1,11 +1,15 @@
 package no.ntnu.tdt4240.g17.common.network.game_messages.data;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by Morten 'bujordet' Bujordet on 3/25/2019.
  *
  * @author Morten 'bujordet' Bujordet
  */
 @SuppressWarnings("VisibilityModifier")
+@AllArgsConstructor @NoArgsConstructor
 public class Position {
     /** Position of the player in x-direction. */
     public float x;
