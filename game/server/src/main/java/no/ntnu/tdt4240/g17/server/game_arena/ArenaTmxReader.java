a00b0c18649de0665d3e6c421d2c27ec80b7ff2e
Kristian Rekstad <krirek@msn.com>
2019-05-02 18:08:51 +0200

    Fix issue with resources and files

--- game/server/src/main/java/no/ntnu/tdt4240/g17/server/game_arena/ArenaTmxReader.java
+++ game/server/src/main/java/no/ntnu/tdt4240/g17/server/game_arena/ArenaTmxReader.java
@@ -7,7 +7,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +30,7 @@ class ArenaTmxReader {
 
     /**
      * Get the tiles in a map.
+     *
      * @param arena the arena to read tiles form
      * @return a list of (x,y) positions of tiles' bottom left corner.
      */
@@ -42,36 +43,37 @@ class ArenaTmxReader {
                 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
                 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
                 final String filePath = ArenaUtil.getFilePathFor(arena);
-                final File arenaFile = new File(getClass().getResource("/" + filePath).toURI());
-                if (arenaFile.exists()) {
-                    Document doc = dBuilder.parse(arenaFile);
-                    doc.getDocumentElement().normalize();
-                    final NodeList layers = doc.getElementsByTagName("layer");
-                    for (int i = 0; i < layers.getLength(); i++) {
-                        final Node item = layers.item(i);
-                        if (item.getNodeType() == Node.ELEMENT_NODE) {
-                            Element element = (Element) item;
-                            final String name = element.getAttribute("name");
-                            if ("map".equals(name)) {
-                                int width = Integer.valueOf(element.getAttribute("width"));
-                                int height = Integer.valueOf(element.getAttribute("height"));
-                                String data = element.getElementsByTagName("data").item(0).getTextContent();
-                                String[] cells = data.replace("\n", "").split(",");
+                try (InputStream arenaResource = getClass().getResourceAsStream("/" + filePath)) {
+                    if (arenaResource != null) {
+                        Document doc = dBuilder.parse(arenaResource);
+                        doc.getDocumentElement().normalize();
+                        final NodeList layers = doc.getElementsByTagName("layer");
+                        for (int i = 0; i < layers.getLength(); i++) {
+                            final Node item = layers.item(i);
+                            if (item.getNodeType() == Node.ELEMENT_NODE) {
+                                Element element = (Element) item;
+                                final String name = element.getAttribute("name");
+                                if ("map".equals(name)) {
+                                    int width = Integer.valueOf(element.getAttribute("width"));
+                                    int height = Integer.valueOf(element.getAttribute("height"));
+                                    String data = element.getElementsByTagName("data").item(0).getTextContent();
+                                    String[] cells = data.replace("\n", "").split(",");
 
-                                for (int cellIndex = 0; cellIndex < cells.length; cellIndex++) {
-                                    if ("0".equals(cells[cellIndex])) {
-                                        continue;
+                                    for (int cellIndex = 0; cellIndex < cells.length; cellIndex++) {
+                                        if ("0".equals(cells[cellIndex])) {
+                                            continue;
+                                        }
+                                        int x = cellIndex % width;
+                                        int y = height - (cellIndex / width) - 1;
+                                        result.add(new Vector2(x, y));
                                     }
-                                    int x = cellIndex % width;
-                                    int y = height - (cellIndex / width) - 1;
-                                    result.add(new Vector2(x, y));
+                                    cache.put(arena, result);
                                 }
-                                cache.put(arena, result);
                             }
                         }
+                    } else {
+                        throw new IllegalStateException("Missing arena file: " + filePath);
                     }
-                } else {
-                    throw new IllegalStateException("Missing arena file: " + filePath);
                 }
             } catch (Exception ex) {
                 log.error("Failed to parse xml for {}", arena.name(), ex);
